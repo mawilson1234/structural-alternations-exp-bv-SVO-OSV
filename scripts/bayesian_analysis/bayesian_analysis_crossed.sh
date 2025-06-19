bc7715aacc711cc8717bed8d1c964caed5284aee
Michael Wilson <michael.a.wilson@yale.edu>
2025-06-19 12:11:31 -0400

    update scripts

--- scripts/bayesian_analysis/bayesian_analysis_crossed.sh
+++ scripts/bayesian_analysis/bayesian_analysis_crossed.sh
@@ -6,8 +6,9 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=16
 #SBATCH --mem-per-cpu=16G
-#SBATCH --partition=day
-#SBATCH --time=01-00:00:00
+#SBATCH --constraint=cascadelake
+#SBATCH --partition=week
+#SBATCH --time=02-00:00:00
 #SBATCH --mail-type=FAIL,END,INVALID_DEPEND
 
 module load R/4.4.1-foss-2022b
