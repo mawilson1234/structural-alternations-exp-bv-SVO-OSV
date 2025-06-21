403caf7beb6bbf65a51e3b4657bbc538bf0eff5b
mawilson1234 <michael.a.wilson@yale.edu>
2025-06-21 00:35:09 -0400

    update names

--- analysis/Bayesian scripts/models/model_crossed_mobertB.r
+++ analysis/Bayesian scripts/models/model_crossed_mobertB.r
@@ -2,7 +2,7 @@ source(file.path('Bayesian scripts', 'bayesian_functions.r'))
 
 fit.model(
 	data.type = 'accuracy',
-	model.type = 'crossed',
+	model.type = 'crossed-mobertB',
 	data.file = 'accuracy-data.csv',
 	data.function = ident,
 	data.sources = c('human', 'ModernBERT Base'),
--- analysis/Bayesian scripts/models/model_nested_bert.r
+++ analysis/Bayesian scripts/models/model_nested_bert.r
@@ -2,7 +2,7 @@ source(file.path('Bayesian scripts', 'bayesian_functions.r'))
 
 fit.model(
 	data.type = 'accuracy',
-	model.type = 'nested',
+	model.type = 'nested-bert',
 	data.file = 'accuracy-data-nested.csv',
 	data.function = ident,
 	data.sources = c('human', 'BERT'),
